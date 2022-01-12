4dd6db127b38db729db99e907e9c2a2dcc6830e9
HenriqueFranxa <hfranca@ua.pt>
2022-01-12 15:30:26 +0000

    botão personalziado nav

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -24,11 +24,11 @@ body {
 
 export const Button = styled.button`
   border-radius: 4px;
-  background: ${({ primary }) => (primary ? '#4B59F7' : '#0467FB')};
+  background: #4B59F7;
   white-space: nowrap;
-  padding: ${({ big }) => (big ? '12px 64px' : '10px 20px')};
+  padding: 12px 32px;
   color: #fff;
-  font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
+  font-size: 15px;
   outline: none;
   border: none;
   cursor: pointer;
@@ -36,7 +36,8 @@ export const Button = styled.button`
   &:hover {
     transition: all 0.3s ease-out;
     background: #fff;
-    background-color: ${({ primary }) => (primary ? '#0467FB' : '#4B59F7')};
+    background-color: #0467FB;
+    transform: scale(0.97);
   }
 `;
 
