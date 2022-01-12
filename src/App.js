19f38dfbd84dbf926fc7fb5abb94da74cfb23893
HenriqueFranxa <hfranca@ua.pt>
2022-01-12 15:23:58 +0000

    versão com nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
-import GlobalStyles from "./components/styles/Global";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import GlobalStyles from "./components/styles/GlobalStyles";
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    header: '#000',
+    body: '#fff',
+    footer: '#003333',
+  },
+}
 
 function App() {
   return (
-    <>
-    <GlobalStyles />
-    <Router>
-      <Navbar />
-    </Router>
-    </>
+
+    <ThemeProvider theme={theme}>
+
+      <>
+        <Router>
+          <GlobalStyles />
+          <Navbar />
+          {/* <Switch>
+        <Route index element={} />
+        <Route path='/' element={} />
+        <Route path='/' element={} />
+        <Route path='/' element={} />
+      </Switch> */}
+        </Router>
+
+        {/* <div>
+        fdsfsdfdsfsd
+        fdsfsdfdsfsd
+        <p>reiiii</p>
+      </div> */}
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtnLink
+} from '../styles/NavbarStyles';
+import { Button } from '../styles/GlobalStyles';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>BoredviserLogo</NavLogo>
+
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='/'>
+                Home
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='/'>
+                Services
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='/'>
+                Products
+              </NavLinks>
+            </NavItem>
+
+            <NavBtnLink to='/'>
+              <Button primary>SIGN UP</Button>
+            </NavBtnLink>
+
+          </NavMenu>
+
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+}
+
+export default Navbar;
