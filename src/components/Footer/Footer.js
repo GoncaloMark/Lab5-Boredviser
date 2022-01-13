01c997cee36c44ddddf01cc0ee25ad9b48bda467
HenriqueFranxa <hfranca@ua.pt>
2022-01-13 17:48:58 +0000

    Meti icones no footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,49 +1,53 @@
 import React from 'react';
-
-import { Box , Container , Column , Row , Flinks , Heading , Copyright} from '../styles/FooterStyles';
+import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaYoutubeSquare } from "react-icons/fa";
+import { Box, Container, Column, Row, Flinks, Heading, Copyright } from '../styles/FooterStyles';
 
 export const Footer = () => {
-  return (
-    <Box>
-      <Container>
-        <Row>
-          <Column>
-            <Heading>About us</Heading>
-            <Flinks>Find more about us</Flinks>
-          </Column>
-          <Column>
-            <Heading>Shops</Heading>
-            <Flinks>Buy products</Flinks>
-            <Flinks>Clothes</Flinks>
-          </Column>
-          <Column>
-            <Heading>Social Media</Heading>
-            <Flinks href='#'>
-              <i className='fab fa-facebook-f'>
-                <span style={{ marginLeft: '10px' }}>Facebook</span>
-              </i>
-            </Flinks>
-            <Flinks href='#'>
-              <i className='fab fa-instagram'>
-                <span style={{ marginLeft: '10px' }}>Instagram</span>
-              </i>
-            </Flinks>
-            <Flinks href='#'>
-              <i className='fab fa-twitter'>
-                <span style={{ marginLeft: '10px' }}>Twitter</span>
-              </i>
-            </Flinks>
-            <Flinks href='#'>
-              <i className='fab fa-youtube'>
-                <span style={{ marginLeft: '10px' }}>Youtube</span>
-              </i>
-            </Flinks>
-          </Column>
-        </Row>
-        <div>
-            <Copyright>© 2022 Boredviser - All Rights Reserved. Gonçalo Marques & Henrique França</Copyright>
-        </div>
-      </Container>
-    </Box>
-  );
+    return (
+        <Box>
+            <Container>
+                <Row>
+                    <Column>
+                        <Heading>About us</Heading>
+                        <Flinks>Find more about us</Flinks>
+                    </Column>
+                    <Column>
+                        <Heading>Shops</Heading>
+                        <Flinks>Buy products</Flinks>
+                        <Flinks>Clothes</Flinks>
+                    </Column>
+                    <Column>
+                        <Heading>Social Media</Heading>
+                        <Flinks href='#'>
+                            <i className='fab fa-facebook-f'>
+                                <FaFacebookSquare />
+                                <span style={{ marginLeft: '10px' }}>Facebook</span>
+                            </i>
+                        </Flinks>
+                        <Flinks href='#'>
+                            <i className='fab fa-instagram'>
+                                <FaInstagramSquare />
+                                <span style={{ marginLeft: '10px' }}>Instagram</span>
+                            </i>
+                        </Flinks>
+                        <Flinks href='#'>
+                            <i className='fab fa-twitter'>
+                                <FaTwitterSquare />
+                                <span style={{ marginLeft: '10px' }}>Twitter</span>
+                            </i>
+                        </Flinks>
+                        <Flinks href='#'>
+                            <i className='fab fa-youtube'>
+                                <FaYoutubeSquare />
+                                <span style={{ marginLeft: '10px' }}>Youtube</span>
+                            </i>
+                        </Flinks>
+                    </Column>
+                </Row>
+                <div>
+                    <Copyright>© 2022 Boredviser - All Rights Reserved. Gonçalo Marques & Henrique França</Copyright>
+                </div>
+            </Container>
+        </Box>
+    );
 };
