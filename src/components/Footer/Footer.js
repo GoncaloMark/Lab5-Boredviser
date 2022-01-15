96b4c1afcc8cd66d82e987e04c18b1450e351cd6
HenriqueFranxa <hfranca@ua.pt>
2022-01-15 03:19:49 +0000

    coloquei icones no footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Container, Column, Row, Flinks, Heading, Copyright } from '../styles/FooterStyles';
+import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa'
 
 export const Footer = () => {
     return (
@@ -12,6 +13,9 @@ export const Footer = () => {
                     </Column>
                     <Column>
                         <Heading>Social Media</Heading>
+                        <Flinks><FaFacebook style={{"margin-right" : "10px"}}/>Facebook</Flinks>
+                        <Flinks><FaTwitter style={{"margin-right" : "10px"}}/>Twitter</Flinks>
+                        <Flinks><FaInstagram style={{"margin-right" : "10px"}}/>Instagram</Flinks>
                     </Column>
                 </Row>
                 <div>
