68a2483de0f48bcc2c48aceb623cb5a05050f1d1
HenriqueFranxa <hfranca@ua.pt>
2022-01-20 22:38:58 +0000

    Final

--- src/components/Paginas/Preferences.js
+++ src/components/Paginas/Preferences.js
@@ -1,12 +1,26 @@
 import React from "react";
 import {useParams} from "react-router-dom";
+import {Container} from "../styles/ContainerStyles";
+import {ButtonC} from "../styles/ButtonStyles";
+import {Footer} from "../Footer/Footer";
+import Rei from "../PreferenceList/PrefList";
 
 function Preferences() {
     const {uid} = useParams()
 
     if(uid)
     {
-        return(<p>User ID: {uid}</p>)
+        return(<div><p>User ID: {uid}</p>
+            
+        <div style={{'display': 'flex','justify-content':'center'}}><Rei /></div>
+        <Container style={{'justify-content':'center'}}>
+            <div style={{'margin':'auto' , 'padding':'0px'}}>
+                
+                <ButtonC>Generate Activities</ButtonC>
+            </div>
+        </Container>
+        <Footer />
+    </div>)
     }
 
 }
