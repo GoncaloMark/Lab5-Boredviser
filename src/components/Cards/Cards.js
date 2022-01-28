4125793649cf2ebc55cc28ecbf66df0a0850a933
GoncaloMark <gmgoncalo7@gmail.com>
2022-01-28 21:52:45 +0000

    Cards finalizados

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -6,27 +6,32 @@ import { ImageCard } from "../styles/ImageStyles";
 function Cards(props) {
 
     const lista = props.h
+    const fotos = props.f
+
+    console.log(lista)
+    console.log(fotos)
+
 
     return (
         <>
         <h2 style={{ 'color': 'white' , 'textAlign':'center', 'margin-bottom': '5px' , 'margin-top': '20px'}}>Activities</h2>
             <Container_Card>
                 <Card_Container>
-                    <ImageCard/>
+                    <ImageCard src={fotos[0].ph}/>
                     <Card_Content>
                         <Card_Title><Card_H3>{lista[0].type}</Card_H3></Card_Title>
                         <Card_P>{lista[0].activity}</Card_P>
                     </Card_Content>
                 </Card_Container>
                 <Card_Container>
-                    <ImageCard/>
+                    <ImageCard src={fotos[1].ph}/>
                     <Card_Content>
                         <Card_Title><Card_H3>{lista[1].type}</Card_H3></Card_Title>
                         <Card_P>{lista[1].activity}</Card_P>
                     </Card_Content>
                 </Card_Container>
                 <Card_Container>
-                    <ImageCard/>
+                    <ImageCard src={fotos[2].ph}/>
                     <Card_Content>
                         <Card_Title><Card_H3>{lista[2].type}</Card_H3></Card_Title>
                         <Card_P>{lista[2].activity}</Card_P>
@@ -34,14 +39,14 @@ function Cards(props) {
                 </Card_Container>
                 
                 <Card_Container>
-                    <ImageCard/>
+                    <ImageCard src={fotos[3].ph}/>
                     <Card_Content>
                         <Card_Title><Card_H3>{lista[3].type}</Card_H3></Card_Title>
                         <Card_P>{lista[3].activity}</Card_P>
                     </Card_Content>
                 </Card_Container>
                 <Card_Container>
-                    <ImageCard/>
+                    <ImageCard src={fotos[4].ph}/>
                     <Card_Content>
                         <Card_Title><Card_H3>{lista[4].type}</Card_H3></Card_Title>
                         <Card_P>{lista[4].activity}</Card_P>
--- src/components/PreferenceList/PrefList.js
+++ src/components/PreferenceList/PrefList.js
@@ -4,16 +4,16 @@ import {Container} from "../styles/ContainerStyles";
 import {ButtonC} from "../styles/ButtonStyles";
 import Cards from "../Cards/Cards.js";
 
-const userData = [
-  { name: "education" },
-  { name: "recreational" },
-  { name: "social" },
-  { name: "diy" },
-  { name: "cooking" },
-  { name: "relaxation" },
-  { name: "music" },
-  { name: "busywork" },
-  { name: "charity" }
+export const userData = [
+  { name: "education", photo:{ph:'https://images.unsplash.com/photo-1503676260728-1c00da094a0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZWR1Y2F0aW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60' , id:"education"}},
+  { name: "recreational", photo:{ph:'https://images.unsplash.com/photo-1565113052401-9683a46de386?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmVjcmVhdGlvbmFsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60' , id: "recreational"}},
+  { name: "social", photo:{ph:'https://images.unsplash.com/photo-1491438590914-bc09fcaaf77a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c29jaWFsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60', id:"social"} },
+  { name: "diy", photo:{ph:'https://images.unsplash.com/photo-1522065893269-6fd20f6d7438?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGl5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60', id:"diy" }},
+  { name: "cooking", photo:{ph:'https://images.unsplash.com/photo-1466637574441-749b8f19452f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29va2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60', id:"cooking" }},
+  { name: "relaxation", photo:{ph:'https://images.unsplash.com/photo-1491172700640-4f1a131a3670?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHJlbGF4YXRpb258ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60', id:"relaxation" }},
+  { name: "music", photo:{ph:'https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bXVzaWN8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60', id:"music" }},
+  { name: "busywork", photo:{ph:'https://media.istockphoto.com/photos/working-over-time-picture-id172475923?b=1&k=20&m=172475923&s=170667a&w=0&h=XBO8E8lezH61xNL-wxTQm1bMKQ64eMbMleLXn3Whhgk=', id:"busywork" }},
+  { name: "charity", photo:{ph:'https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2hhcml0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',  id:"charity"}}
 ];
 
 function Exception(message)
@@ -31,15 +31,48 @@ function Rei()
   const [control, setControl] = useState(0)
   const [text, setText] = useState('Generate Activities')
   const[Arr, setArr] = useState([])
+  const[PH, setPH] = useState([])
 
-  async function GetData(types, setLoading, setc)
+  function sortResponse(a, b)
+  {
+    const labelA = a.type
+    const labelB = b.type
+
+     if(labelA < labelB)
+     {
+       return -1
+     }  else if(labelA > labelB)
+     {
+       return 1
+     } else return 0
+  }
+
+  function sortPH(a, b)
+  {
+    const phA = a.id
+    const phB = b.id
+
+    if(phA < phB)
+    {
+      return -1
+    } else if(phA > phB)
+    {
+      return 1
+    } else return 0
+  }
+
+
+
+  async function GetData(types, setLoading, setc, photos)
   {
     setLoading(true)
     await types.forEach(
         (value) => fetch("http://www.boredapi.com/api/activity?type=" + value)
             .then(response => response.json())
-            .then((response) => Arr.push(response))
+            .then((response) => {Arr.push(response); Arr.sort(sortResponse)})
     )
+
+    photos.forEach((e) => {PH.push(e); PH.sort(sortPH)})
 
     setTimeout(function(){
       setLoading(false);
@@ -53,19 +86,21 @@ function Rei()
 
   }, []);
 
-  let ArrayMap = UserData.map((i) => ({ label: i.name, value: i.name }))
+  let ArrayMap = UserData.map((i) => ({ label: i.name, value: i.name, photo: i.photo }))
 
   function Destructuring(data)
   {
-    let {value: value1} = data[0]
-    let {value: value2} = data[1]
-    let {value: value3} = data[2]
-    let {value: value4} = data[3]
-    let {value: value5} = data[4]
+    let {value: value1, photo: photo1} = data[0]
+    let {value: value2, photo: photo2} = data[1]
+    let {value: value3, photo: photo3} = data[2]
+    let {value: value4, photo: photo4} = data[3]
+    let {value: value5, photo: photo5} = data[4]
 
     const types = [value1, value2, value3, value4, value5]
+    const photos = [photo1, photo2, photo3, photo4, photo5]
 
-    GetData(types, setLoading, setControl)
+
+    GetData(types, setLoading, setControl, photos)
   }
 
   if( control>0 && loading===false) {
@@ -73,7 +108,7 @@ function Rei()
     return (
         <div style={{'marginTop':'0px' , 'marginBottom':'15px'}}>
           
-            <Cards h={Arr}/>
+            <Cards h={Arr} f={PH}/>
           
         </div>
     );
